24fd2f6f6ed25621125f3e67013362ec5e47f2b4
John-507 <jambzz53@gmail.com>
2023-11-10 17:08:01 -0500

    add hardware back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Share } from '@capacitor/share';
+
+
+import {IonRouterOutlet, Platform } from '@ionic/angular';
+import { App } from '@capacitor/app';
+
 
 
 @Component({
@@ -8,28 +13,33 @@ import { Share } from '@capacitor/share';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  
+  @ViewChild(IonRouterOutlet) outlet;
+
   public appPages = [
     { title: 'Home', url: '/home', icon: 'home' },
     { title: 'Customers', url: '/customers', icon: 'people-circle' },
     { title: 'Provincias', url: '/provincias', icon: 'location' },
     { title: 'Login', url: '/login', icon: 'log-in' },
     { title: 'Scanner', url: '/scanner', icon: 'qr-code' }
-    
+
   ];
-  
-  constructor(
 
-  ) {}
+  constructor(private platform: Platform) {
+    this.platform.backButton.subscribeWithPriority(-1, () => {
+      if (!this.outlet?.canGoBack()) {
+        App.exitApp();
+      }
+    });
 
-  shareApp(){
+  }
+
+  shareApp() {
     Share.share({
       title: 'See cool stuff',
       text: 'Really awesome thing you need to see right meow',
       url: 'http://ionicframework.com/'
 
     });
-
   }
 
 
